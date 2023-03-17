c76843447e051bb2ce063221019b417e3c9f22f7
Harman <singh9765h@gmail.com>
2023-03-17 17:36:57 +0530

    Day 12

---
+++ Day12/main.py
@@ -0,0 +1,28 @@
+#String Slicing & Operations on String
+
+
+
+#Length of a String -> We can find the length of a string using len() function.
+
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a", len1, "letter word.")
+
+name=len("Harman")
+print(name)
+
+#SLICING
+
+names="harman, Singh"
+
+print(names[0:5])
+print(names[:5])
+print(names[2:6])
+print(names[0:-3])
+
+apple="Mango"
+print(apple[-3:-1])  #This gets equivalent to 2:4
+
+
+nm="Harry"
+print(nm[-4:-2])
