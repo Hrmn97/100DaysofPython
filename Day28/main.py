d582b3965d08aa29d0cb8eb59a6c0ac5470bf2c1
Harman <singh9765h@gmail.com>
2023-03-20 14:56:08 +0530

    Day 28

---
+++ Day28/main.py
@@ -0,0 +1,27 @@
+# STRINGS
+
+# String formatting in python
+# String formatting can be done in python using the format method.
+
+# letter = "Hey my nae is {} and i am from {}"
+
+# country ="India"
+# name="harman"
+
+
+# print(letter.format(name,country))
+
+
+price = 49.0999
+
+txt = f"For only Rs {price:.2f} dollars!"
+print(txt)
+
+
+val = 'Geeks'  
+print(f"{val}for{val} is a portal for {val}.")  
+name = 'Tushar'  
+age = 23  
+print(f"Hello, My name is {name} and I'm {age} years old.")
+
+
