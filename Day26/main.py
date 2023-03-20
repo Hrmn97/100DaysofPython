5e96e5705478a0211fd7671b3ee71498cde6fa3e
Harman <singh9765h@gmail.com>
2023-03-20 13:56:15 +0530

    Day 26

---
+++ Day26/main.py
@@ -0,0 +1,22 @@
+import time
+timestamp = time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp = time.strftime('%H')
+# print(timestamp)
+# timestamp = time.strftime('%M')
+# print(timestamp)
+# timestamp = time.strftime('%S')
+# print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
+
+
+
+hour = int(time.strftime('%H'))
+print(hour)
+
+if(hour>0 and hour<12):
+    print("Good morning sir")
+elif(hour>12 and hour<17):
+    print("Good after noon sir")
+elif(hour>=17 and hour<0):
+    print("Good night sir")
