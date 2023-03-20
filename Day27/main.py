2e68b7f9decea9134ba3aecab311734cf17da2d1
Harman <singh9765h@gmail.com>
2023-03-20 14:55:54 +0530

    Day 27

---
+++ Day27/main.py
@@ -0,0 +1,12 @@
+# Create a program capable of displaying questions to the user like KBC. 
+# Use List data type to store the questions and their correct answers. 
+# Display the final amount the person is taking home after playing the game.
+
+# questions = ["What is first letter of alphabets /nA) d /nB) c /nC) a /nD) b","How many stated in india /nA) 22 /nB) 28 /nC) 90 /nD) 29","What is 2+2 /nA) 8 /nB) 2 /nC) 4 /nD) 0 ","What is 9 - 8 /nA) 1 /nB) 2 /nC) 9 /nD) 19"]
+
+# answers = ['C', 'B', 'C', 'A']
+# x = 0 
+# for i in questions:
+#     print(i)
+#     a1 = input("Enter your answer:")
+#     if(a1 == answers[x])
